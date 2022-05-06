f40a98a9313d67fb202fc510460c0b6f185c9134
Ymshow <71683873+Ymshub@users.noreply.github.com>
2022-05-06 13:50:28 +0800

    Update Stream_Check.js

--- JavaScript/Stream_Check.js
+++ JavaScript/Stream_Check.js
@@ -8,7 +8,7 @@ const REQUEST_HEADERS = {
   let panel_result = {
     title: ' Stream Check',
     content: '',
-    icon: 'play.tv.fill',
+    icon: '4k.tv',
     'icon-color': '#FF2D55',
   }
   await Promise.all([check_netflix(), check_youtube_premium()])
