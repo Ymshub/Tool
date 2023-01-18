458364114d6655baeb6adfeed23ef8c80ca47da0
Ymshow <71683873+Ymshub@users.noreply.github.com>
2023-01-18 14:53:36 +0800

    Update TimeCard.js

--- JavaScript/TimeCard.js
+++ JavaScript/TimeCard.js
@@ -87,52 +87,9 @@ function datenotice() {
     //console.log("当日已通知");
   }
 }
-
-//>图标依次切换乌龟、兔子、闹钟、礼品盒
-function icon_now(num){
-  if(num<=7 && num>3 ){
-    return "hare"
-  }else if(num<=3 && num>0){
-    return "alarm"
-  }else if(num==0){
-    return "gift"
-  }else{
-    return "tortoise"
-  }
-}
-//>图标颜色
-function icon_color(num){
-  if(num<=7 && num>3){
-    return '#F0FCFF'
-  }else if(num<=3 && num>0){
-    return '#EA5506'
-  }else if(num==0){
-    return '#BE002F'
-  }else{
-    return '#00BC12'
-  }
-}
 $done({
-title:title_random(tnumcount(Number(nowlist))),
-icon:icon_now(tnumcount(Number(nowlist))),
-content:tlist[nowlist][0]+":"+today(tnumcount(nowlist))+"天,"+tlist[Number(nowlist) + Number(1)][0] +":"+ tnumcount(Number(nowlist) + Number(1))+ "天,"+tlist[Number(nowlist) + Number(2)][0]+":"+tnumcount(Number(nowlist) + Number(2))+"天"
+title:"节假提醒",
+icon:"list.dash.header.rectangle",
+'icon-color': "#5AC8FA",
+content:tlist[nowlist][0]+"  :  "+today(tnumcount(nowlist))+"天\n"+tlist[Number(nowlist) + Number(1)][0] +"  :  "+ tnumcount(Number(nowlist) + Number(1))+ "天\n"+tlist[Number(nowlist) + Number(2)][0]+"  :  "+tnumcount(Number(nowlist) + Number(2))+"天"
 })
-
-function title_random(num){
-  let r = Math.floor((Math.random()*10)+1);
-  let dic = {
-    1:"距离放假，还要摸鱼多少天？🥱",
-    2:"坚持住，就快放假啦！💪",
-    3:"上班好累呀，好想放假😮‍💨",
-    4:"努力，我还能加班24小时！🧐",
-    5:"天呐，还要多久才放假呀？😭",
-    6:"躺平中，等放假☝(՞ਊ ՞)☝",
-    7:"只有摸鱼才是赚老板的钱🙎🤳",
-    8:"一起摸鱼吧✌(՞ټ՞ )✌",
-    9:"摸鱼中，期待下一个假日.ʕʘ‿ʘʔ.",
-    10: "小乌龟慢慢爬🐢",
-    11:"太难了！😫😩😖(´◉‿◉)",
-    12:"反正放假也不能去玩！😤"
-  };
-  return num==0?"节日快乐🎉，万事大吉🥳":dic[r]
-}
